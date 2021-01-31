9b56ab5344ef111713c69812afbd14beb3295d19
“Asha <ashaprasad1710@gmail.com>
2021-01-31 14:09:19 +0000

    test python plot

---
+++ limbic-system-model/plot_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pylab as pl
+#
+data = np.loadtxt('v5ctxneuron_test.dat');
+#
+fig = pl.figure(1)
+#
+a = 0
+b = len(data[:,0])
+# compound
+pl.subplot(311);
+pl.plot(data[a:b,0],data[a:b,1]);
+pl.plot(data[a:b,0],data[a:b,2]);
+pl.plot(data[a:b,0],data[a:b,3]);
+pl.ylabel('R-IG-IB');
+#
+pl.subplot(312);
+pl.plot(data[a:b,0],data[a:b,5]);
+pl.plot(data[a:b,0],data[a:b,6]);
+pl.ylabel('w OFC G/B');
+#
+pl.subplot(313);
+pl.plot(data[a:b,0],data[a:b,4]);
+pl.ylabel('OFC');
+#
+#
+pl.show();
+
+
